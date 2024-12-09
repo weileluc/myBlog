1f12fadf254d205b8be7a1988d6b9ea159b843c6
weileluc <weileluc@students.zhaw.ch>
2024-12-09 17:42:00 +0100

    test5

--- src/lib/components/BlogBox.svelte
+++ src/lib/components/BlogBox.svelte
@@ -8,7 +8,7 @@
             <img src={blog.titleImage} alt="" class="title-image"/>
         </div>
         <div class="col-lg-7 blog-text">
-            <h1><a class="link active" href="/">{blog.title}</a></h1>
+            <h1><a class="link active" href="/blogs/{blog._id}">{blog.title}</a></h1>
             <p>{blog.subtitle}</p>
             <p>{blog.autor}</p>
         </div>
---
+++ src/routes/blogs/[blog_id]/+page.server.js
@@ -0,0 +1,8 @@
+
+
+export async function load ({params}) {
+
+    return {
+       
+    }
+}
