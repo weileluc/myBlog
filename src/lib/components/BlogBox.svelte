248c2c6a72d44f7a89d2ec3d7a1c1c86d7dee22f
weileluc <weileluc@students.zhaw.ch>
2024-12-11 08:13:21 +0100

    margin l und r

--- src/lib/components/BlogBox.svelte
+++ src/lib/components/BlogBox.svelte
@@ -2,12 +2,12 @@
     let { blog } = $props();
 </script>
 
-<div class="blog-box">
+<div class="blogs-box">
     <div class="row">
         <div class="col-lg-5 blogs-image">
             <img src={blog.titleImage} alt="" class="title-image"/>
         </div>
-        <div class="col-lg-7 blog-text">
+        <div class="col-lg-7 blogs-text">
             <h1><a class="link active" href="/blogs/{blog._id}">{blog.title}</a></h1>
             <p>{blog.subtitle}</p>
             <p>{blog.autor}</p>
