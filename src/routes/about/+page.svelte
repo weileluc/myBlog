81c57c43dce9ff685273f388f5839ea699c62d37
weileluc <weileluc@students.zhaw.ch>
2025-01-03 10:27:05 +0100

    css

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,16 +2,15 @@
 
 <div class="about-box">
     <p>
-        Hallo und willkommen auf meinem Reiseblog! Ich bin <strong
-            >Luca</strong
-        >, sehr reisebegeistert und abenteuerlustig, der seine Erlebnisse mit anderen
-        teilt. Hier findest du Geschichten, Tipps und Eindrücke von meinen
-        Reisen rund um die Welt.
+        Hallo und willkommen auf meinem Reiseblog! Ich bin <strong>Luca</strong
+        >, sehr reisebegeistert und abenteuerlustig, der seine Erlebnisse mit
+        anderen teilt. Hier findest du Geschichten, Tipps und Eindrücke von
+        meinen Reisen rund um die Welt.
     </p>
     <p>
         Ob spannende Roadtrips, versteckte Orte oder die schönsten Naturwunder –
         ich nehme dich mit auf meine Reise. Lass uns gemeinsam die Welt
         entdecken!
     </p>
-    <img src="/images/w3cr.jpg" alt="" class="about-image" />
+    <img src="/images/w3cr.jpg" alt="Reisebild von Luca" class="about-image" />
 </div>
